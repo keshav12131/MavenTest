15a55e1dbd6f117de4679edd1bf57cbdf9849b46
keshav <keshav@example.com>
2020-10-12 19:58:18 +0530

    Commit for pipeline project1

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World123!" );
-        System.out.println( "New msg" );
+        System.out.println( "New msg1" );
     }
 }
